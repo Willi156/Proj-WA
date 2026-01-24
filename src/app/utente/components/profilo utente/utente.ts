6e20151ef6a1c2855d778dd766e41fc58cfad8b2
alessiasica31-byte <alessiasica31@gmail.com>
2026-01-24 10:28:24 +0100

    aggiunta recensioni e preferiti

--- src/app/utente/components/profilo utente/utente.ts
+++ src/app/utente/components/profilo utente/utente.ts
@@ -12,16 +12,36 @@ import { RouterLink } from '@angular/router';
 })
 export class UtenteComponent {
 
-
   user = {
     nome: 'Alessia',
     cognome: 'Sica',
     username: 'alessia_dev',
-    email: 'alessia@studenti.unical.it'
+    email: 'alessia@studenti.unical.it',
+
+    preferiti: [
+      { titolo: 'Stranger Things', tipo: 'Serie TV', cover: '📺' },
+      { titolo: 'Harry Potter', tipo: 'Film', cover: '⚡' },
+      { titolo: 'Interstellar', tipo: 'Film', cover: '🚀' },
+      { titolo: 'The Witcher', tipo: 'Serie TV', cover: '⚔️' }
+    ],
+
+    recensioni: [
+      { oggetto: 'Inception', voto: 5, testo: 'Un capolavoro assoluto, trama incredibile!' },
+      { oggetto: 'Barbie', voto: 4, testo: 'Molto colorato e divertente, ma finale strano.' }
+    ]
   };
 
-  salvaModifiche() {
-    console.log('Dati salvati:', this.user);
-    alert('Dati salvati (simulazione)!');
+
+  rimuoviPreferito(index: number) {
+    if(confirm("Vuoi davvero rimuovere questo preferito?")) {
+      this.user.preferiti.splice(index, 1);
+    }
+  }
+
+
+  rimuoviRecensione(index: number) {
+    if(confirm("Vuoi cancellare questa recensione?")) {
+      this.user.recensioni.splice(index, 1);
+    }
   }
 }
