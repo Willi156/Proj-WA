2d012a8245d92c381ecf355db946a32f37f577d3
alessiasica31-byte <alessiasica31@gmail.com>
2026-01-25 23:21:53 +0100

    aggiornamenti

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,8 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environment/environment';
-
 import { Game } from '../games/models/game.model';
 import { Film } from '../film/model/film.model';
-import {SerieTv} from '../serieTV/model/serie-tv.model';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
@@ -13,22 +10,42 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  // Endpoint di test che abbiamo nel backend: /api/test
+  authenticate(username: string, password: string) {
+    return this.http.post<any>(
+      `${this.baseUrl}/api/auth/login`,
+      { username, password },
+      { withCredentials: true }
+    );
+  }
+
+  getCurrentUserInfo() {
+    return this.http.get<any>(`${this.baseUrl}/api/auth/me`, { withCredentials: true });
+  }
+
+
+  getFavouritesMediaByUserId(userId: number) {
+    return this.http.get<any[]>(`${this.baseUrl}/api/utente/${userId}/preferiti`, { withCredentials: true });
+  }
+
+  getFavouriteMediaByUserIdComplete(userId: number) {
+    return this.http.get<any[]>(`${this.baseUrl}/api/utente/${userId}/preferitiCompleti`, { withCredentials: true });
+  }
+
+  getRecensioniByUserId(userId: number) {
+    return this.http.get<any[]>(`${this.baseUrl}/api/recensioni/utente/${userId}`, { withCredentials: true });
+  }
+
+
   getServerTime() {
     return this.http.get<{ serverTime: { now: string } }>(`${this.baseUrl}/api/test`);
   }
 
   getFirstUser() {
-  return this.http.get<any>(`${this.baseUrl}/api/utente/first`);
-}
-
- authenticate(username: string, password: string) {
-    return this.http.post<{ token: string }>(`${this.baseUrl}/api/auth/login`, { username, password });
+    return this.http.get<any>(`${this.baseUrl}/api/utente/first`);
   }
 
-  newContenuto(titolo: string, descrizione: string, genere: string, link: string, tipo: string, annoPubblicazione: number, casaProduzione?: string, casaEditrice?: string,inCorso?: boolean, stagioni?: number) {
-    console.log('Creating new contenuto with casa produzione:', { casaProduzione});
-    return this.http.post<{ id: number }>(`${this.baseUrl}/api/newContenuto`, { titolo, descrizione, genere, link, tipo, annoPubblicazione, casaProduzione, casaEditrice, inCorso, stagioni });
+  newContenuto(titolo: string, descrizione: string, genere: string, link: string, tipo: string, annoPubblicazione: number, casaProduzione?: string, casaEditrice?: string, inCorso?: boolean, stagioni?: number) {
+    return this.http.post<{ id: number }>(`${this.baseUrl}/api/newContenuto`, { titolo, descrizione, genere, link, tipo, annoPubblicazione, casaProduzione, casaEditrice, inCorso, stagioni }, { withCredentials: true });
   }
 
   getContenuti() {
@@ -48,34 +65,16 @@ export class ApiService {
   }
 
   getRecensioniByContenutoId(contenutoId: number) {
-    return this.http.get<any[]>(`${this.baseUrl}/api/recensioni/contenuto`,{params: {contenutoId}});
+    return this.http.get<any[]>(`${this.baseUrl}/api/recensioni/contenuto`, { params: { contenutoId } });
   }
 
   getCheckUsername(username: string) {
     return this.http.get<{ available: boolean }>(`${this.baseUrl}/api/utente/checkUsernameExists`, { params: { username } });
   }
+
   createUser(nome: string, cognome: string, username: string, password: string, email: string) {
     return this.http.post<{ id: number }>(`${this.baseUrl}/api/newUtente`, { nome, cognome, email, username, password });
   }
-  getFavouritesMediaByUserId(userId: number) {
-    return this.http.get<any[]>(`${this.baseUrl}/api/utente/${userId}/preferiti`);
-  }
-  getFavouriteMediaByUserIdComplete(userId: number) {
-    return this.http.get<any[]>(`${this.baseUrl}/api/utente/${userId}/preferitiCompleti`);
-  }
-
-  getRecensioniByUserId(userId: number) {
-    return this.http.get<any[]>(`${this.baseUrl}/api/recensioni/utente/${userId}`);
-  }
-
-
-
-
-
-
-
-
-
 
   // Esempio per futuri endpoint:
   // getItems() { return this.http.get<Item[]>(`${this.baseUrl}/api/items`); }
--- src/app/utente/components/profilo utente/utente.ts
+++ src/app/utente/components/profilo utente/utente.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../../services/api.service';
-import {empty} from 'rxjs';
+import { empty } from 'rxjs';
+
 @Component({
   selector: 'app-utente',
   standalone: true,
@@ -11,78 +12,78 @@ import {empty} from 'rxjs';
   templateUrl: './utente.html',
   styleUrl: './utente.css'
 })
-export class UtenteComponent implements OnInit{
-  userId=2;
-  preferitiIds:any []=[];
-  recensioniIds:any []=[];
+export class UtenteComponent implements OnInit {
+  userId = 2;
+  preferitiIds: any[] = [];
+  recensioniIds: any[] = [];
 
-  constructor(private api:ApiService) {
-  }
-
-
-  ngOnInit():void {
-    //chiamata per il recupero dei preferiti
-    this.api.getFavouriteMediaByUserIdComplete(this.userId).subscribe({
-      next: (result: any[]) => {
-        console.log('Preferiti ricevuti:', result);
-        this.preferitiIds = result;
-      },
-      error: (err) => {
-        console.error('Errore nel recupero dei contenuti:', err);
-      }
-    })
-
-    //chiamata per il recupero delle recensioni, (lato html va aggiunto)
-    this.api.getRecensioniByUserId(this.userId).subscribe({
-      next: (result: any[]) => {
-        console.log('Preferiti ricevuti:', result);
-        this.recensioniIds = result;
-      },
-      error: (err) => {
-        console.error('Errore nel recupero dei contenuti:', err);
-      }
-    })
-  }
-
-
-
-
-
-
+  user = {
+    nome: '',
+    cognome: '',
+    username: '',
+    email: '',
+    immagineProfilo: ''
+  };
 
   activeTab: string = 'games';
   searchText: string = '';
 
-  user = {
-    nome: 'Alessia',
-    cognome: 'Sica',
-    username: 'alessia_dev',
-    email: 'alessia@studenti.unical.it',
+  constructor(private api: ApiService, private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.api.authenticate('WIMAn', '123456!').subscribe({
+      next: (response) => {
+        if (response) {
+          if (response.id) this.userId = response.id;
+
+          const datiReali = response.user || response;
+
+          this.user = {
+            nome: datiReali.nome || 'Nome Assente',
+            cognome: datiReali.cognome || '',
+            username: datiReali.username || '',
+            email: datiReali.email || '',
+            immagineProfilo: datiReali.immagineProfilo || ''
+          };
+
+          if (datiReali.recensioni) {
+            this.recensioniIds = datiReali.recensioni;
+          }
+
+          this.cd.detectChanges();
+        }
+
+        this.caricaDati();
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
   }
 
+  caricaDati() {
+    this.api.getCurrentUserInfo().subscribe({
+      next: (userInfo) => {
+        if (userInfo && userInfo.recensioni) {
+          this.recensioniIds = userInfo.recensioni;
+          this.cd.detectChanges();
+        }
+      },
+      error: () => { }
+    });
 
+    this.api.getFavouriteMediaByUserIdComplete(this.userId).subscribe({
+      next: (result: any[]) => {
+        this.preferitiIds = result;
+        this.cd.detectChanges();
+      },
+      error: () => { }
+    });
+  }
 
   setActiveTab(tabName: string) {
     this.activeTab = tabName;
   }
 
-  /**get filteredPreferiti() {
-    if (!this.searchText) {
-      return this.user.preferiti;
-    }
-    return this.user.preferiti.filter(item =>
-      item.titolo.toLowerCase().includes(this.searchText.toLowerCase())
-    );
-  }
-
-  rimuoviRecensione(index: number) {
-    if(confirm("Eliminare questa recensione?")) {
-      this.user.recensioni.splice(index, 1);
-    }
-  }
-
-  rimuoviPreferito(itemDaRimuovere: any) {
-    this.user.preferiti = this.user.preferiti.filter(item => item !== itemDaRimuovere);
-  }*/
   protected readonly empty = empty;
 }
