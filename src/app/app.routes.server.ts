73e31325a9cc323f37f323f7e867cb4d571e1dcf
Willi156 <williarduino@outlook.it>
2026-02-08 08:56:20 -0800

    inserito trailer

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'modifica-contenuto/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:kind/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
